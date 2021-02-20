b1cf0cee6d340d03917503bbd5c991b4b6cc784a
andi balo <andibalo213@gmail.com>
2021-02-21 01:33:55 +0700

    set up media migration and model, create post endpoint to upload image

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const isBase64 = require("is-base64");
+const base64Img = require("base64-img");
+const { Media } = require("../models");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/", async (req, res) => {
+  const { image } = req.body;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({ error: "image is not base64" });
+  }
+
+  await base64Img.img(
+    image,
+    "./public/images",
+    Date.now(),
+    async (err, filePath) => {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+
+      const filename = filePath.split("\\").pop().split("/").pop();
+
+      const media = await Media.create({ image: `image/${filename}` });
+
+      return res.json({
+        data: {
+          id: media.id,
+          image: `${req.get("host")}/images/${filename}`,
+        },
+      });
+    }
+  );
 });
 
 module.exports = router;
